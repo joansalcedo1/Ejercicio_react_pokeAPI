0313b63e3baf6e6669e9c805bbe3f8a90d19b248
joansalcedo1 <sebastiansalcedo7@gmail.com>
2025-08-14 00:29:15 -0500

    segundo commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 export default function Hero() {
     return (
         <>
-            <section className={`p-4 text-center w-full h-screen`}>
+            <section className={`p-4 text-center`}>
                 <div className="row rounded m-4 shadow-lg d-flex">
                     <div className="col m-5 p-auto ">
-                        <h1 className="pb-5">Primer proyecto de react con boostrap</h1>
-                        <p>Este es el primer proyecto de react con vite y boostrap.
-                            Fue diseñado bajo el marco del bootcamp de <b>programación
-                                orientada el desarrollo web full stack </b>de valle talento
-                            tech. El cual duro más de 140 horas
+                        <h1 className="pb-5">¡Atrapa a todos los Pokémon desde tu navegador!
+                        </h1>
+                        <p>Explora el mundo Pokémon con datos en tiempo real gracias a la PokeAPI.
+                            Descubre sus estadísticas, habilidades y más, todo en una interfaz rápida y divertida.
+
                         </p>
                     </div>
                     <div className="col m-5 p-4">
